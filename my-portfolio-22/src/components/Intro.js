c3a53550c5ddd3ea14cc55cc1bfa34ce01df7616
Tove Adamsson <toveadamsson@Toves-MBP.home>
2022-11-11 13:21:32 +0100

    contact form text added

--- my-portfolio-22/src/components/Intro.js
+++ my-portfolio-22/src/components/Intro.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../css/intro.scss";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/intro.scss'
 // import About from "../components/About";
-export default function Intro() {
+export default function Intro () {
   return (
-    <div className="main-container">
-      <div className="background">
-        <div className="animated-title">
-          <div className="text-top">
+    <div className='main-container'>
+      <div className='background'>
+        <div className='animated-title'>
+          <div className='text-top'>
             <div>
               <span>Hello! I'm Tove Adamsson</span>
               <span>a Software Engineer</span>
             </div>
           </div>
-          <div className="text-bottom">
+          <div className='text-bottom'>
             <div>
-              I appreciate you for checking in!
-              <br /> Welcome to my{" "}
-              <Link className="button" to="/about">
+              <span> I appreciate you for checking in!</span>
+              <span>Welcome to my  <Link className="button" to="/about">
                 Portfolio
-              </Link>
+              </Link></span>
+   
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
